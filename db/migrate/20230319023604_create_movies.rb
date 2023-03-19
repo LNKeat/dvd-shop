3f70aff746b053f74476433a5f846531507b7499
Laura <LNKEAT@gmail.com>
2023-03-18 20:39:23 -0600

    set up dvd-shop lab

---
+++ db/migrate/20230319023604_create_movies.rb
@@ -0,0 +1,18 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :length
+      t.string :integer
+      t.string :director
+      t.string :description
+      t.string :poster_url
+      t.string :category
+      t.boolean :discount
+      t.boolean :female_director
+
+      t.timestamps
+    end
+  end
+end
